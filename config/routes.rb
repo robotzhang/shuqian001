057b32c2bc81465e87bdd4852de5dd0848a74c66
robotzhang <445679586@qq.com>
2013-07-23 00:49:56 +0800

    合集管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shuqian001::Application.routes.draw do
   root :to => 'application#index'
+  post 'links' => 'links#create'
   resources :collections, :path => 'heji' do
     resources :links
   end
