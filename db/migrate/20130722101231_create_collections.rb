032be4fb7e39be9e5c35ae7fa478bc3b8379d9ed
robotzhang <robotzhang@qq.com>
2013-07-22 18:31:57 +0800

    database init

---
+++ db/migrate/20130722101231_create_collections.rb
@@ -0,0 +1,11 @@
+# 资源合集
+class CreateCollections < ActiveRecord::Migration
+  def change
+    create_table :collections do |t|
+      t.string :title # 合集标题
+      t.string :description # 补充描述
+      t.references :user # 所属用户
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20130722101643_create_links.rb
@@ -0,0 +1,14 @@
+# 链接(资源)表
+class CreateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.string :title # 链接标题
+      t.string :url # 链接地址
+      t.string :status # 链接状态（审核通过，待审核）
+
+      t.references :user # 所属用户
+      t.references :collection # 所属合集
+      t.timestamps
+    end
+  end
+end
