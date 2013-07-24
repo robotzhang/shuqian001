eb946ccabb191118853b6acfddc2cec2ceab6502
robotzhang <robotzhang@qq.com>
2013-07-24 17:22:51 +0800

    add links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,6 +5,7 @@ class LinksController < ApplicationController
 
   def create
     @link = Link.new_from_url(params[:link][:url])
+    @link.collection_id = params[:link][:collection_id]
     @link.save
     respond_to do |format|
       format.js
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,11 +1,12 @@
 class Link < ActiveRecord::Base
   attr_accessible :title, :url, :collection_id
   belongs_to :collection
-  #validates :title, presence: true, uniqueness: { scope: :collection_id }
+  validates :title, presence: true, uniqueness: { scope: :collection_id }
   validates :url, presence: true, uniqueness: { scope: :collection_id }
 
   def self.new_from_url(url)
     link = Link.new
+    return link if url.blank?
     page = Nokogiri::HTML(open(url))
     link.url = url
     link.title = page.css("title").text
