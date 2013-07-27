6e0b9108a3689a13c8b568ab4a1ef838c5625397
robotzhang <445679586@qq.com>
2013-07-28 03:21:00 +0800

    链接显示方式

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 #require 'openssl'
 #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'timeout'
 
 class Link < ActiveRecord::Base
   attr_accessible :title, :url, :collection_id
@@ -11,15 +12,22 @@ class Link < ActiveRecord::Base
     link = Link.new
     return link if url.blank?
 
-    page = Nokogiri::HTML(open(url))
     link.url = url
-    link.title = page.css("title").text
-    description = page.xpath("//meta[translate(
-      @name,
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-      'abcdefghijklmnopqrstuvwxyz'
-    ) = 'keywords']/@content")
-    link.description = description.blank? ? "" : description.first.value # 大小写不相关
+    begin
+      timeout(3) do # 3秒
+        page = Nokogiri::HTML(open(url))
+
+        link.title = page.css("title").text
+        description = page.xpath("//meta[translate(
+          @name,
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+          'abcdefghijklmnopqrstuvwxyz'
+        ) = 'keywords']/@content")
+        link.description = description.blank? ? "" : description.first.value # 大小写不相关
+      end
+    rescue Timeout::Error
+      return link
+    end
 
     link
   end
--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(:version => 20130727071513) do
   end
 
   create_table "users", :force => true do |t|
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
     t.string   "email",                  :default => "",       :null => false
     t.string   "encrypted_password",     :default => "",       :null => false
     t.string   "reset_password_token"
