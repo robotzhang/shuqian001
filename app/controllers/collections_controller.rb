a3acf35510e5d07fb20f86366aa5cc46a45fbaf0
robotzhang <robotzhang@qq.com>
2013-07-30 18:11:22 +0800

    编辑合集

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,3 +1,4 @@
+#coding=utf-8
 class CollectionsController < ApplicationController
   load_and_authorize_resource
 
@@ -12,11 +13,16 @@ class CollectionsController < ApplicationController
   end
 
   def edit
-
+    @collection = Collection.find(params[:id])
   end
 
   def update
-
+    @collection = Collection.find(params[:id])
+    if @collection.update_attributes(params[:collection])
+      redirect_to(collection_path(@collection), notice: "成功更新合集："+@collection.title)
+    else
+      render(:edit)
+    end
   end
 
   def show
