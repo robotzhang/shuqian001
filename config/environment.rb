f2630e1849ce6c19c6a20e97fc4890b21133e7e0
robotzhang <robotzhang@qq.com>
2013-08-06 14:43:10 +0800

    邮件的配置

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.qq.com",
     :port => 25,
-    :authentication => :login,
+    :authentication => "plain",
     :user_name => "robotzhang@qq.com",
     :password => "ZhangRobot"   #用户名和密码是163邮箱的，需要验证
 }
