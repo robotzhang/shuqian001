580a5b4f02565e0a34b54f58d33841463e93d965
robotzhang <445679586@qq.com>
2013-08-08 23:49:24 +0800

    devise的sender要和mailer中的一致，否则不能发送

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "robotzhang@qq.com"
+  config.mailer_sender = "no-repay@shuqian001.com"
 
   # Configure the class responsible to send e-mails.
   config.mailer = "Devise::Mailer"
