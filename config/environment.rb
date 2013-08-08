50c0289e09e2737dbd602e7e693411e275776b8b
robotzhang <robotzhang@qq.com>
2013-08-08 16:25:19 +0800

    qq企业邮箱代发测试

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.exmail.qq.com",
     :port => 25,
-    :authentication => "plain",
+    :authentication => :login,
     :user_name => "no-repay@shuqian001.com",
     :password => "ShuQian001"   #用户名和密码是163邮箱的，需要验证
 }
