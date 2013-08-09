1e62c32e3649beb3de73d209382b77a0c60bb047
robotzhang <robotzhang@qq.com>
2013-08-09 15:45:09 +0800

    投票的唯一性确认

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,12 @@
+#coding=utf-8
 class Vote < ActiveRecord::Base
   attr_accessible :vote
   belongs_to :votable, :polymorphic => true
+  before_save do
+    scope = Vote.where(:user_id => self.user_id, :votable_type => self.votable_type, :votable_id => self.votable_id)
+    if scope.count > 0
+      self.errors.add(:vote, '已经投票')
+      false
+    end
+  end
 end
