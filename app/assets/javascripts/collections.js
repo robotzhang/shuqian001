d9a81d484c2ec0c0ce1c0351b50c22146b1d7688
robotzhang <445679586@qq.com>
2013-08-28 05:11:57 +0800

    增加标签选择和系统推荐标签

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -10,6 +10,25 @@ $(document).ready(function() {
   $('#tags').tagsInput({
     'defaultText': '',
     'width':'100%',
-    'height':'40px'
+    'height':'40px',
+    "onRemoveTag": function(tag) {
+      $.each($('.sys_tags a'), function(){
+        if ($(this).text() == tag) {
+          $(this).removeClass("selected");
+          return false;
+        }
+      });
+    }
+  });
+
+  // 选择标签
+  $('.sys_tags a').click(function() {
+    if ($(this).hasClass("selected")) {
+      $(this).removeClass("selected");
+      $('#tags').removeTag($(this).text());
+    } else {
+      $(this).addClass("selected");
+      $('#tags').addTag($(this).text());
+    }
   });
 });
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,4 @@
+#coding=utf-8
 class Collection < ActiveRecord::Base
   acts_as_commentable
   acts_as_taggable
@@ -13,11 +14,17 @@ class Collection < ActiveRecord::Base
   belongs_to :user
 
   before_save :set_tag_owner
+
   # 设置打标签的用户
   def set_tag_owner
     # Set the owner of some tags based on the current tag_list
     set_owner_tag_list_on(User.find(self.user_id), :tags, self.tag_list)
     # Clear the list so we don't get duplicate taggings
     self.tag_list = nil # 如果不这样会出现重复标签
+  end
+
+  # 系统标签
+  def self.sys_tags
+    ["技术", "电影", "音乐", "读书", "游戏", "小说", "设计", "博文", "软件", "学习", "创意", "互联网", "品牌"]
   end
 end
