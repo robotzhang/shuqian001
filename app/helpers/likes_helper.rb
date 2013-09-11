cc580f4db0670cee690b354373b742834cf002cf
zhangxiaohua <445679586@qq.com>
2013-09-11 21:18:16 +0800

    增加link中的收藏和评论链接

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,9 @@
 module LikesHelper
+  def link_do_like(object, text="收藏")
+    link_to object.likes.size.to_s+text.html_safe,
+            likes_path(object.class.to_s.underscore, object.id),
+            remote: true,
+            class: "muted",
+            :method => "post"
+  end
 end
