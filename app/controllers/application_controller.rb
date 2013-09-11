8ecf30504fb4070cd3f4f754ff90beb333824af1
zhangxiaohua <445679586@qq.com>
2013-09-11 18:12:02 +0800

    页头头像撑破div的问题

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
-      format.html { redirect_to(root_url, :alert => exception.message) }
+      format.html { redirect_to(new_user_session_path(back: request.url), :alert => "请先登陆以继续该操作") }
       format.js {render :js => "alert('请先登录');"}
     end
   end
