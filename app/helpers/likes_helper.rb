76b2ef9b1500726d4c41682d04dc966a5aad3685
zhangxiaohua <445679586@qq.com>
2013-09-12 14:05:38 +0800

    标签模块的css样式

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,9 +1,17 @@
 module LikesHelper
   def link_do_like(object, text="收藏")
-    link_to object.likes.size.to_s+text.html_safe,
+    link_to object.likes.count.to_s+text.html_safe,
             likes_path(object.class.to_s.underscore, object.id),
             remote: true,
             class: "muted",
+            :method => "post"
+  end
+
+  def button_do_like(object, text="关注该标签")
+    link_to text.html_safe,
+            likes_path(object.class.to_s.underscore, object.id),
+            remote: true,
+            class: "btn btn-success btn-block",
             :method => "post"
   end
 end
