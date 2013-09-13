2ea99946ed9b7ce78bff765c5613e79c2a448291
zhangxiaohua <445679586@qq.com>
2013-09-14 00:14:34 +0800

    like的count cache

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,9 @@
 #coding=utf-8
 class Like < ActiveRecord::Base
   # attr_accessible :title, :body
-  belongs_to :likable, :polymorphic => true, :counter_cache => true
+  belongs_to :likable, :polymorphic => true
+  belongs_to :link, :foreign_key => :commentable_id, :class_name => "Link", :counter_cache => true
+  belongs_to :tag, :foreign_key => :commentable_id, :class_name => "Tag", :counter_cache => true
   belongs_to :user
   before_save do
     scope = Like.where(:user_id => self.user_id, :likable_type => self.likable_type, :likable_id => self.likable_id)
