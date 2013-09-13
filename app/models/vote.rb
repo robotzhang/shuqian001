fda342a4314f5534727484f1c3c41a4c983ed6fd
zhangxiaohua <445679586@qq.com>
2013-09-14 00:09:21 +0800

    count cache 1对多关系下的处理

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,8 @@
 #coding=utf-8
 class Vote < ActiveRecord::Base
   attr_accessible :vote
-  belongs_to :votable, :polymorphic => true, :counter_cache => true
+  belongs_to :votable, :polymorphic => true
+  belongs_to :link, :foreign_key => :commentable_id, :class_name => "Link", :counter_cache => true
   before_save do
     scope = Vote.where(:user_id => self.user_id, :votable_type => self.votable_type, :votable_id => self.votable_id)
     if scope.count > 0
